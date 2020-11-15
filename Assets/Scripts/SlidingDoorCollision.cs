7eeb07923e80b8fea856f87dc9fee4584056edd8
Unknown <zadj@create.aau.dk>
2020-11-15 18:43:20 +0100

    Door slides open when approached after imperative stimulus

--- Assets/Scripts/SlidingDoorCollision.cs
+++ Assets/Scripts/SlidingDoorCollision.cs
@@ -2,31 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlidingDoor : MonoBehaviour
+public class SlidingDoorCollision : MonoBehaviour
 {
+    public GameObject Manager;
     public GameObject Door;
+    private Manager m;
+    public Vector3 zeroPosition;
     private Color originalColor;
-    private Collider doorCollider;
-    public bool Colliding;
     private float time = 0f;
     public Color NoGoColor;
     public Color GoColor;
-    public GameObject Manager;
-    private Manager m;
     public bool doneChange;
-
+    //private float time = 0f;
     // Start is called before the first frame update
     void Start()
     {
         m = Manager.GetComponent<Manager>();
+        zeroPosition = Door.transform.position;
+
         originalColor = Door.GetComponent<Renderer>().material.color;
-        doorCollider = Door.GetComponent<Collider>();
         doneChange = false;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {        
         if (m.shownImperative && !doneChange){
             ColourChanging(m.GoNoGoState);
         }
@@ -37,14 +37,12 @@ public class SlidingDoor : MonoBehaviour
         }
     }
 
-private void OnTriggerEnter(Collider other) {
-    Colliding = true;
-    Door.transform.position = Vector3.Lerp(Door.transform.position, new Vector3(1.67f, 0, 0), time += Time.deltaTime/2);
-}
+    private void OnTriggerEnter(Collider other) {
+        if (m.shownImperative){
+            StartCoroutine(SlideDoor());
+        }
+    }
 
- private void OnTriggerExit(Collider other) {
-    Colliding = false;
-}
     void ColourChanging(string GoNoGo)
     {
         if (GoNoGo == "NoGo" && 1 > time){
@@ -54,5 +52,13 @@ private void OnTriggerEnter(Collider other) {
             Door.GetComponent<Renderer>().material.color = Color.Lerp(GoColor, originalColor, time += Time.deltaTime/2);
         }
         if (time <= 0) {doneChange = true;}
+    }
+
+    IEnumerator SlideDoor(){
+        for (float f = 0; f < 1; f += 0.005f)
+        {
+            Door.transform.position = Vector3.Lerp(Door.transform.position, new Vector3(2f, 0, 0), f);
+            yield return new WaitForSeconds(.005f);
+        }
     }
 }
