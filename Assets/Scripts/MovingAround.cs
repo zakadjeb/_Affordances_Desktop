5b913c021cffc9b9922b6e3d4df77a7876aa8c2b
Unknown <zadj@create.aau.dk>
2020-11-19 09:46:49 +0100

    Mouse deactivated; now only WASD and Arrows work. A and D rotates, W and D goes back and forth.

--- Assets/Scripts/MovingAround.cs
+++ Assets/Scripts/MovingAround.cs
@@ -5,39 +5,49 @@ using UnityEngine;
 public class MovingAround : MonoBehaviour
 {
 
-    private float MovementX;
+    //private float MovementX;
     private float MovementY;
+    private float Turning;
     [SerializeField] [Range(0f, 10f)] public float MovingSpeed;
-    [SerializeField] [Range(0f, 10f)] public float MouseSpeed;
+    [SerializeField] [Range(0f, 100f)] public float TurningSpeed;
     public float PlayerHeight = 1.8f;
 
-    private float MouseX = 0.0f;
-    private float MouseY = 0.0f;
+    // private float MouseX = 0.0f;
+    // private float MouseY = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        MouseX = 180f;
+        transform.position = new Vector3(-2.5f,PlayerHeight,0f);
+        Turning = 180f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MovementX = Input.GetAxis ("Horizontal");
+        //MovementX = Input.GetAxis ("Horizontal");
         MovementY = Input.GetAxis ("Vertical");
 
-        if (MovementY != 0 || MovementX != 0) 
+        if (MovementY != 0) 
         {
+            //transform.position += transform.right * MovementX * Time.deltaTime * MovingSpeed;
             transform.position += transform.forward * MovementY * Time.deltaTime * MovingSpeed;
-            transform.position += transform.right * MovementX * Time.deltaTime * MovingSpeed;
             transform.position = new Vector3(transform.position.x, PlayerHeight, transform.position.z);
         }
 
-        if (Input.GetMouseButton(0)){
-            MouseX += MouseSpeed * Input.GetAxis("Mouse X");
-            MouseY += MouseSpeed * Input.GetAxis("Mouse Y");
-            transform.eulerAngles = new Vector3(MouseY, MouseX, 0.0f);
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+            Turning += Time.deltaTime * TurningSpeed;
         }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+            Turning -= Time.deltaTime * TurningSpeed;
+        }
+        transform.eulerAngles = new Vector3(0.0f, Turning, 0.0f);
+
+        // if (Input.GetMouseButton(0)){
+        //     MouseX += MouseSpeed * Input.GetAxis("Mouse X");
+        //     MouseY += MouseSpeed * Input.GetAxis("Mouse Y");
+        //     transform.eulerAngles = new Vector3(MouseY, MouseX, 0.0f);
+        // }
         
     }
 
